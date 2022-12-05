93b186ccfcde16dc296b7b8a7fdb16d56768cf61
Roy Storey <imlejiii@example.com>
2022-12-05 15:39:50 -0500

    Work at work. Lots of JSON stuff.

--- js/anychart.js
+++ js/anychart.js
@@ -1,86 +1,396 @@
+// var obj = {
+//   "missionsupportsystems": {
+//     "airgap": {
+//       "navaid": {
+//         "name": "NAVAID/Flightline Equipment",
+//         "poc": "Joe",
+//         "description": "This is mission support systems...",
+//         "completionItems": ["develop", "design", "defend", "give up"],
+//         "color": "#790000"
+//       },
+//       "upgrademedia": {
+//         "name": "Upgrade Media",
+//         "poc": "Joe",
+//         "description": "This is mission support systems...",
+//         "completionItems": ["develop", "design", "defend", "give up"],
+//         "color": "#790000"
+//       },
+//       "missionplanning": {
+//         "name": "Mission Planning",
+//         "poc": "Joe",
+//         "description": "This is mission support systems...",
+//         "completionItems": ["develop", "design", "defend", "give up"],
+//         "color": "#790000"
+//       },
+//       "name": "Air Gap",
+//       "poc": "Joe",
+//       "description": "This is mission support systems...",
+//       "completionItems": ["develop", "design", "defend", "give up"],
+//       "color": "#790000"
+//     },
+//     "directconnect": {
+//       "efb": {
+//         "name": "EFB",
+//         "poc": "Joe",
+//         "description": "This is mission support systems...",
+//         "completionItems": ["develop", "design", "defend", "give up"],
+//         "color": "#0b5a00"
+//       },
+//       "mxlaptops": {
+//         "name": "Mx Laptops",
+//         "poc": "Joe",
+//         "description": "This is mission support systems...",
+//         "completionItems": ["develop", "design", "defend", "give up"],
+//         "color": "#8f8c00"
+//       },
+//       "name": "Direct Connect",
+//       "poc": "Joe",
+//       "description": "This is mission support systems...",
+//       "completionItems": ["develop", "design", "defend", "give up"],
+//       "color": "#790000"
+//     },
+//     "name": "Mission Support Systems",
+//     "poc": "Joe",
+//     "description": "This is mission support systems...",
+//     "completionItems": ["develop", "design", "defend", "give up"],
+//     "color": "#790000"
+//   },
+//   "transport":{
+//     "services": {
+//       "sln": {
+//         "name": "SLN",
+//         "poc": "Joe",
+//         "description": "This is mission support systems...",
+//         "completionItems": ["develop", "design", "defend", "give up"],
+//         "color": "#790000"
+//       },"cms": {
+//         "name": "CMS",
+//         "poc": "Joe",
+//         "description": "This is mission support systems...",
+//         "completionItems": ["develop", "design", "defend", "give up"],
+//         "color": "#790000"
+//       },"jwics": {
+//         "name": "JWICS",
+//         "poc": "Joe",
+//         "description": "This is mission support systems...",
+//         "completionItems": ["develop", "design", "defend", "give up"],
+//         "color": "#790000"
+//       },
+//       "name": "Services",
+//       "poc": "Joe",
+//       "description": "This is mission support systems...",
+//       "completionItems": ["develop", "design", "defend", "give up"],
+//       "color": "#790000"
+//     },
+//     "satcom": {
+//       "milcom": {
+//         "name": "MILCOM",
+//         "poc": "Joe",
+//         "description": "This is mission support systems...",
+//         "completionItems": ["develop", "design", "defend", "give up"],
+//         "color": "#790000"
+//       },
+//       "viasat": {
+//         "name": "VIASAT",
+//         "poc": "Joe",
+//         "description": "This is mission support systems...",
+//         "completionItems": ["develop", "design", "defend", "give up"],
+//         "color": "#790000"
+//       },
+//       "satcomdirect": {
+//         "name": "SATCOM Direct",
+//         "poc": "Joe",
+//         "description": "This is mission support systems...",
+//         "completionItems": ["develop", "design", "defend", "give up"],
+//         "color": "#790000"
+//       },
+//       "name": "SATCOM",
+//       "poc": "Joe",
+//       "description": "This is mission support systems...",
+//       "completionItems": ["develop", "design", "defend", "give up"],
+//       "color": "#790000"
+//     },
+//     "gateway": {
+//       "eacn": {
+//         "name": "EACN",
+//         "poc": "Joe",
+//         "description": "This is mission support systems...",
+//         "completionItems": ["develop", "design", "defend", "give up"],
+//         "color": "#0b5a00"
+//       },
+//       "dvhomenetwork": {
+//         "name": "DV Home Network",
+//         "poc": "Joe",
+//         "description": "This is mission support systems...",
+//         "completionItems": ["develop", "design", "defend", "give up"],
+//         "color": "#790000"
+//       },
+//       "viasat": {
+//         "name": "VIASAT",
+//         "poc": "Joe",
+//         "description": "This is mission support systems...",
+//         "completionItems": ["develop", "design", "defend", "give up"],
+//         "color": "#790000"
+//       },
+//       "name": "Gateway",
+//       "poc": "Joe",
+//       "description": "This is mission support systems...",
+//       "completionItems": ["develop", "design", "defend", "give up"],
+//       "color": "#790000"
+//     },
+//     "name": "Transport",
+//         "poc": "Joe",
+//         "description": "This is mission support systems...",
+//         "completionItems": ["develop", "design", "defend", "give up"],
+//         "color": "#333333"
+//   },
+//   "customer":{
+//     "travelkit": {
+//       "hostdevices": {
+//         "name": "Host Devices",
+//         "poc": "Joe",
+//         "description": "This is mission support systems...",
+//         "completionItems": ["develop", "design", "defend", "give up"],
+//         "color": "#790000"
+//       },
+//       "networkdevices": {
+//         "name": "Network Devices",
+//         "poc": "Joe",
+//         "description": "This is mission support systems...",
+//         "completionItems": ["develop", "design", "defend", "give up"],
+//         "color": "#790000"
+//       },
+//       "name": "Travel Kit",
+//         "poc": "Joe",
+//         "description": "This is mission support systems...",
+//         "completionItems": ["develop", "design", "defend", "give up"],
+//         "color": "#333333"
+//     },
+//     "personal": {
+//       "personaldevices": {
+//         "name": "Personal Devices",
+//         "poc": "Joe",
+//         "description": "This is mission support systems...",
+//         "completionItems": ["develop", "design", "defend", "give up"],
+//         "color": "#790000"
+//       },
+//       "name": "Personal",
+//       "poc": "Joe",
+//       "description": "This is mission support systems...",
+//       "completionItems": ["develop", "design", "defend", "give up"],
+//       "color": "#790000"
+//     },
+//     "name": "Customer",
+//       "poc": "Joe",
+//       "description": "This is mission support systems...",
+//       "completionItems": ["develop", "design", "defend", "give up"],
+//       "color": "#333333"
+//   },
+//   "aircraftnetwork":{
+//     "mcs": {
+//       "hostdevices": {
+//         "name": "Host Devices",
+//         "poc": "Joe",
+//         "description": "This is mission support systems...",
+//         "completionItems": ["develop", "design", "defend", "give up"],
+//         "color": "#790000"
+//       },
+//       "networkdevices": {
+//         "name": "Network Devices",
+//         "poc": "Joe",
+//         "description": "This is mission support systems...",
+//         "completionItems": ["develop", "design", "defend", "give up"],
+//         "color": "#790000"
+//       },
+//       "name": "MCS",
+//       "poc": "Joe",
+//       "description": "This is mission support systems...",
+//       "completionItems": ["develop", "design", "defend", "give up"],
+//       "color": "#790000"
+//     },
+//     "avionicsbus": {
+//       "arinc429": {
+//         "name": "ARINC-429",
+//         "poc": "Joe",
+//         "description": "This is mission support systems...",
+//         "completionItems": ["develop", "design", "defend", "give up"],
+//         "color": "#790000"
+//       },
+//       "name": "Avionics Bus",
+//       "poc": "Joe",
+//       "description": "This is mission support systems...",
+//       "completionItems": ["develop", "design", "defend", "give up"],
+//       "color": "#790000"
+//     },
+//     "name": "Aircraft Network",
+//         "poc": "Joe",
+//         "description": "This is mission support systems...",
+//         "completionItems": ["develop", "design", "defend", "give up"],
+//         "color": "#333333"
+//   }
+// }
+
+function include(file)
+{
+  var script = document.createElement('script');
+  script.src=file;
+  script.type='text/javascript';
+  script.defer = true
+
+  document.getElementsByTagName('head').item(0).appendChild(script);
+}
+
+let test = include('/json/accessData.json')
+
+console.log(test)
+
 anychart.onDocumentLoad(function () {
+  
+  // fetch("/json/accessData.json")
+  // .then(response => response.json())
+  // })
+  // .then(accessData => {
+  //   return accessData
+  // });
+
+  // async function accessData(){
+  //   const response = await fetch('/json/accessData.json');
+  //   const dataFromJson = await response.json();
+  //   console.log(dataFromJson);
+  //   return(dataFromJson);
+  // }
+  // accessData()
+  // var stringifiedJson = JSON.stringify(accessData());
+  // console.log(stringifiedJson)
+  // console.log(dataFromJson)
+
+  //Declaring variables to reduce redundundancy in typing.
+let mss = obj.missionsupportsystems;
+let airgap = mss.airgap;
+let directconnect = mss.directconnect;
+
+let transport = obj.transport;
+let services = transport.services;
+let satcom = transport.satcom;
+let gateway = transport.gateway;
+
+let customer = obj.customer;
+let travelkit = customer.travelkit;
+let personal = customer.personal;
+
+let aircraftnetwork = obj.aircraftnetwork;
+let mcs = aircraftnetwork.mcs;
+let avionicsbus = aircraftnetwork.avionicsbus;
+
+
+
+
   var data = [
     {
-      name: "Transport", normal: { fill: '#575757' }, children: [
+      name: mss.name, normal: { fill: mss.color }, children: [
         {
-          name: "Gateway", children: [
-            { name: "EACN", normal: { fill: '#096d00' } },
-            { name: "DV Home Network" },
-            { name: "VIASAT" }
+          name: airgap.name, children: [
+            { name: airgap.missionplanning.name },
+            { name: airgap.upgrademedia.name },
+            { name: airgap.navaid.name }
           ]
         },
         {
-          name: "SATCOM", children: [
-            { name: "MILCOM" },
-            { name: "VIASAT" },
-            { name: "SATCOM Direct" }
+          name: directconnect.name, children: [
+            { name: directconnect.efb.name, normal: { fill: directconnect.efb.color } },
+            { name: directconnect.mxlaptops.name, normal: { fill: directconnect.mxlaptops.color } }
+          ]
+         
+        },
+      ]
+      
+    },
+    {
+      name:transport.name, normal: { fill: transport.color }, children: [
+        {
+          name: services.name, children: [
+            { name: services.sln.name },
+            { name: services.cms.name },
+            { name: services.jwics.name }
+          ]
+          
+        },
+        {
+          name: satcom.name, children: [
+            { name: satcom.milcom.name},
+            { name: satcom.viasat.name },
+            { name: satcom.satcomdirect.name }
           ]
         },
         {
-          name: "Services", children: [
-            { name: "SLN" },
-            { name: "CMS" },
-            { name: "JWICS" }
+          name: gateway.name, children: [
+            { name: gateway.eacn.name, normal: { fill: gateway.eacn.color } },
+            { name: gateway.dvhomenetwork.name},
+            { name: gateway.viasat.name }
           ]
         }
       ]
+     
     },
     {
-      name: "Customer", normal: { fill: '#575757' }, children: [
+      name: customer.name, normal: { fill: customer.color }, children: [
         {
-          name: "Personal", normal: { fill: '#c20000' }, children: [
-            { name: "Personal Devices" }
+          name: travelkit.name, children: [
+            { name: travelkit.hostdevices.name },
+            { name: travelkit.networkdevices.name}
+          ] 
+        },
+        {
+          name: personal.name, normal: { fill: personal.color }, children: [
+            { name: personal.personaldevices.name }
+          ]
+          
+        },
+      ]
+     
+    },
+    {
+      name: aircraftnetwork.name, normal: { fill: aircraftnetwork.color }, children: [
+        {
+          name: mcs.name, children: [
+            { name: mcs.hostdevices.name },
+            { name: mcs.networkdevices.name }
           ]
         },
         {
-          name: "Travel Kit", children: [
-            { name: "Host Devices" },
-            { name: "Network Devices" }
+          name: avionicsbus.name, normal: { fill: avionicsbus.color }, children: [
+            { name: avionicsbus.arinc429.name }
           ]
         },
       ]
+      
     },
-    {
-      name: "Aircraft Network", normal: { fill: '#575757' }, children: [
-        {
-          name: "MCS", children: [
-            { name: "Host Devices" },
-            { name: "Network Devices" }
-          ]
-        },
-        {
-          name: "Avionics Bus", normal: { fill: '#c20000' }, children: [
-            { name: "ARINC-429" }
-          ]
-        },
-      ]
-    },
-    {
-      name: "Mission Support Systems", normal: { fill: '#c20000' }, children: [
-        {
-          name: "Direct Connect", children: [
-            { name: "EFB", normal: { fill: '#096d00' } },
-            { name: "Mx Laptops", normal: { fill: '#dbb300' } }
-          ]
-        },
-        {
-          name: "Air Gap", children: [
-            { name: "Mission Planning" },
-            { name: "Uprage Media" },
-            { name: "NAVAID/Flightline Equip" }
-          ]
-        },
-      ]
-    },
-  ];
+  ]
+;
   // create an instance of a pie chart
   var chart = anychart.sunburst(data, "as-tree");
   // changes text to circular rather than radial
   chart.labels().position("radial");
-  chart.labels().fontFamily("Candara");
-  chart.labels().fontSize(100);
-  // chart.labels().wordWrap("normal");
-  chart.labels().height('100px');
+  // changes size of leaves, balances the size of outermost leaves.
+  chart.calculationMode("ordinal-from-leaves");
+  // adjustfontsize is default, causes you to not be able to resize font
+  chart.labels().adjustFontSize(false);
+  chart.labels().wordWrap('break-word');
+  chart.level().labels().fontSize(15);
+  chart.labels().padding(0);
+  // makes circle in middle of chart for plane me to put plane into chart.
+  chart.innerRadius(95);
+  // this is the text in the middle of the page.
+  var label = anychart.standalones.label();
+  label.text("Airplane Picture");
+  label.hAlign("center");
+  label.vAlign("middle");
+  label.width("100%");
+  label.height("100%");
+  label.fontColor("#dd2c00");
+  label.fontSize(12);
+  label.fontWeight(600);
+  chart.center().content(label)
   // set the container element 
   chart.container("chartTwo");
   // initiate chart display
