cbbdff60314c6174ffc03d6462ef2a995063105b
Roy Storey <imlejiii@example.com>
2022-12-06 09:31:50 -0500

    JSON work. Work on Onclick showing data.

--- json/accessData.js
+++ json/accessData.js
@@ -1,31 +1,31 @@
-{"missionsupportsystems": {
+accessData = {"missionsupportsystems": {
   "airgap": {
     "navaid": {
       "name": "NAVAID/Flightline Equipment",
       "poc": "Joe",
       "description": "This is mission support systems...",
       "completionItems": ["develop", "design", "defend", "give up"],
-      "color": "red"
+      "color": "#790000"
     },
     "upgrademedia": {
       "name": "Upgrade Media",
       "poc": "Joe",
       "description": "This is mission support systems...",
       "completionItems": ["develop", "design", "defend", "give up"],
-      "color": "red"
+      "color": "#790000"
     },
     "missionplanning": {
       "name": "Mission Planning",
       "poc": "Joe",
       "description": "This is mission support systems...",
       "completionItems": ["develop", "design", "defend", "give up"],
-      "color": "red"
+      "color": "#790000"
     },
     "name": "Air Gap",
     "poc": "Joe",
     "description": "This is mission support systems...",
     "completionItems": ["develop", "design", "defend", "give up"],
-    "color": "red"
+    "color": "#790000"
   },
   "directconnect": {
     "efb": {
@@ -33,26 +33,26 @@
       "poc": "Joe",
       "description": "This is mission support systems...",
       "completionItems": ["develop", "design", "defend", "give up"],
-      "color": "green"
+      "color": "#0b5a00"
     },
     "mxlaptops": {
       "name": "Mx Laptops",
       "poc": "Joe",
       "description": "This is mission support systems...",
       "completionItems": ["develop", "design", "defend", "give up"],
-      "color": "yellow"
+      "color": "#8f8c00"
     },
     "name": "Direct Connect",
     "poc": "Joe",
     "description": "This is mission support systems...",
     "completionItems": ["develop", "design", "defend", "give up"],
-    "color": "red"
+    "color": "#790000"
   },
   "name": "Mission Support Systems",
   "poc": "Joe",
   "description": "This is mission support systems...",
   "completionItems": ["develop", "design", "defend", "give up"],
-  "color": "red"
+  "color": "#790000"
 },
 "transport":{
   "services": {
@@ -61,25 +61,25 @@
       "poc": "Joe",
       "description": "This is mission support systems...",
       "completionItems": ["develop", "design", "defend", "give up"],
-      "color": "red"
+      "color": "#790000"
     },"cms": {
       "name": "CMS",
       "poc": "Joe",
       "description": "This is mission support systems...",
       "completionItems": ["develop", "design", "defend", "give up"],
-      "color": "red"
+      "color": "#790000"
     },"jwics": {
       "name": "JWICS",
       "poc": "Joe",
       "description": "This is mission support systems...",
       "completionItems": ["develop", "design", "defend", "give up"],
-      "color": "red"
+      "color": "#790000"
     },
     "name": "Services",
     "poc": "Joe",
     "description": "This is mission support systems...",
     "completionItems": ["develop", "design", "defend", "give up"],
-    "color": "red"
+    "color": "#790000"
   },
   "satcom": {
     "milcom": {
@@ -87,27 +87,27 @@
       "poc": "Joe",
       "description": "This is mission support systems...",
       "completionItems": ["develop", "design", "defend", "give up"],
-      "color": "red"
+      "color": "#790000"
     },
     "viasat": {
       "name": "VIASAT",
       "poc": "Joe",
       "description": "This is mission support systems...",
       "completionItems": ["develop", "design", "defend", "give up"],
-      "color": "red"
+      "color": "#790000"
     },
     "satcomdirect": {
       "name": "SATCOM Direct",
       "poc": "Joe",
       "description": "This is mission support systems...",
       "completionItems": ["develop", "design", "defend", "give up"],
-      "color": "red"
+      "color": "#790000"
     },
     "name": "SATCOM",
     "poc": "Joe",
     "description": "This is mission support systems...",
     "completionItems": ["develop", "design", "defend", "give up"],
-    "color": "red"
+    "color": "#790000"
   },
   "gateway": {
     "eacn": {
@@ -115,33 +115,33 @@
       "poc": "Joe",
       "description": "This is mission support systems...",
       "completionItems": ["develop", "design", "defend", "give up"],
-      "color": "green"
+      "color": "#0b5a00"
     },
     "dvhomenetwork": {
       "name": "DV Home Network",
       "poc": "Joe",
       "description": "This is mission support systems...",
       "completionItems": ["develop", "design", "defend", "give up"],
-      "color": "red"
+      "color": "#790000"
     },
     "viasat": {
       "name": "VIASAT",
       "poc": "Joe",
       "description": "This is mission support systems...",
       "completionItems": ["develop", "design", "defend", "give up"],
-      "color": "red"
+      "color": "#790000"
     },
     "name": "Gateway",
     "poc": "Joe",
     "description": "This is mission support systems...",
     "completionItems": ["develop", "design", "defend", "give up"],
-    "color": "red"
+    "color": "#790000"
   },
   "name": "Transport",
       "poc": "Joe",
       "description": "This is mission support systems...",
       "completionItems": ["develop", "design", "defend", "give up"],
-      "color": "gray"
+      "color": "#333333"
 },
 "customer":{
   "travelkit": {
@@ -150,20 +150,20 @@
       "poc": "Joe",
       "description": "This is mission support systems...",
       "completionItems": ["develop", "design", "defend", "give up"],
-      "color": "red"
+      "color": "#790000"
     },
     "networkdevices": {
       "name": "Network Devices",
       "poc": "Joe",
       "description": "This is mission support systems...",
       "completionItems": ["develop", "design", "defend", "give up"],
-      "color": "red"
+      "color": "#790000"
     },
     "name": "Travel Kit",
       "poc": "Joe",
       "description": "This is mission support systems...",
       "completionItems": ["develop", "design", "defend", "give up"],
-      "color": "gray"
+      "color": "#333333"
   },
   "personal": {
     "personaldevices": {
@@ -171,19 +171,19 @@
       "poc": "Joe",
       "description": "This is mission support systems...",
       "completionItems": ["develop", "design", "defend", "give up"],
-      "color": "red"
+      "color": "#790000"
     },
     "name": "Personal",
     "poc": "Joe",
     "description": "This is mission support systems...",
     "completionItems": ["develop", "design", "defend", "give up"],
-    "color": "red"
+    "color": "#790000"
   },
   "name": "Customer",
     "poc": "Joe",
     "description": "This is mission support systems...",
     "completionItems": ["develop", "design", "defend", "give up"],
-    "color": "gray"
+    "color": "#333333"
 },
 "aircraftnetwork":{
   "mcs": {
@@ -192,20 +192,20 @@
       "poc": "Joe",
       "description": "This is mission support systems...",
       "completionItems": ["develop", "design", "defend", "give up"],
-      "color": "red"
+      "color": "#790000"
     },
     "networkdevices": {
       "name": "Network Devices",
       "poc": "Joe",
       "description": "This is mission support systems...",
       "completionItems": ["develop", "design", "defend", "give up"],
-      "color": "red"
+      "color": "#790000"
     },
     "name": "MCS",
     "poc": "Joe",
     "description": "This is mission support systems...",
     "completionItems": ["develop", "design", "defend", "give up"],
-    "color": "red"
+    "color": "#790000"
   },
   "avionicsbus": {
     "arinc429": {
@@ -213,18 +213,18 @@
       "poc": "Joe",
       "description": "This is mission support systems...",
       "completionItems": ["develop", "design", "defend", "give up"],
-      "color": "red"
+      "color": "#790000"
     },
     "name": "Avionics Bus",
     "poc": "Joe",
     "description": "This is mission support systems...",
     "completionItems": ["develop", "design", "defend", "give up"],
-    "color": "red"
+    "color": "#790000"
   },
   "name": "Aircraft Network",
       "poc": "Joe",
       "description": "This is mission support systems...",
       "completionItems": ["develop", "design", "defend", "give up"],
-      "color": "gray"
+      "color": "#333333"
 }
 }
